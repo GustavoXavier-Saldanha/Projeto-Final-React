5fd23ff81044cc25920a7823fd0fe96391f6ea85
Gustavo Xavier <gustavosaldxav@gmail.com>
2021-06-23 13:51:01 -0300

    Login formatado

--- src/Projeto/Paginas/Login/index.jsx
+++ src/Projeto/Paginas/Login/index.jsx
@@ -1,10 +1,31 @@
-import FormularioLogin from "../../Componentes/FormularioLogin"
+
 
 const Login = () =>{
     return(
        
-        <FormularioLogin/>
-       
+        <div className="container">
+        <h2>Login:</h2>
+        <div className="row">
+            <div className="col-12 col-lg-6">
+                <form >
+                    <div className="form-group">
+                        <label>Nome:</label>
+                        <input className="form-control" type="password" placeholder="Digite sua email:" required
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Email:</label>
+                        <input className="form-control" type="password" placeholder="Digite sua senha:" required
+                        />
+                    </div>
+                    
+                    <div className="form-group">
+                        <button className="btn btn-primary mt-3 block">Entrar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
     )
 }
 export default Login
