9e9360c7d8f7e9968e3fce46a1dd2c83edc76554
Gustavo Xavier <gustavosaldxav@gmail.com>
2021-06-23 11:03:54 -0300

    Atualização

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { useState } from 'react'
+import Navbar from './Projeto/Componentes/NavBar'
+import Pagina404 from './Projeto/Paginas/Pagina404'
 
 function App() {
+
+  const [token, setToken] = useState('')
+  const onLogin = (token) => {
+    setToken(token)
+  }
+
+  const logout = () => {
+    setToken('')
+  }
+
   return (
     <div className="App">
-      
+      <BrowserRouter>
+      <Navbar token={token} aoLogout={logout}/>
+      <div className="container">
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route>
+            <Pagina404 />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
     </div>
   );
 }
