ef89dab3205ad83da74e64592b70076b4fb5ad71
Matheus M. Reis <mmr94@live.com>
2021-06-24 10:41:53 -0300

    alteração na home

--- src/Projeto/Componentes/Cards/CardProdutos.jsx
+++ src/Projeto/Componentes/Cards/CardProdutos.jsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom"
 
 
-const CardProduto = ({ nome, preco,descricao,categoria, id}) => { 
+const CardProduto = ({ nome, preco,descricao,imagem_base64,categoria, id}) => { 
     return (
         <div class="row row-cols-1 row-cols-md-3 g-4">
             <div class="col">
                 <div class="card">
-                    <img src="" class="card-img-top" alt="..."/>
+                    <img src={imagem_base64} class="card-img-top" alt="..."/>
                         <div class="card-body">
                             <h5 class="card-title">{nome} - R$ {preco}</h5>
                                  <p class="card-text">{descricao}</p>
--- src/Projeto/Paginas/Home/index.jsx
+++ src/Projeto/Paginas/Home/index.jsx
@@ -10,37 +10,62 @@ const Home = () => {
 
     return (
         <div className="home">
-            <img src={img1} className="Logo-Tela"/>
-        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-            </div>
+            <img src={img1} className="Logo-Tela" />
+            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
 
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src={img2} class="d-block w-100" alt="..." />
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src={img2} class="d-block w-100" alt="..." />
+                    </div>
+                    <div class="carousel-item">
+                        <img src={img3} class="d-block w-100" alt="..." />
+                    </div>
+                    <div class="carousel-item">
+                        <img src={img4} class="d-block w-100" alt="..." />
+                    </div>
                 </div>
-                <div class="carousel-item">
-                    <img src={img3} class="d-block w-100" alt="..." />
-                </div>
-                <div class="carousel-item">
-                    <img src={img4} class="d-block w-100" alt="..." />
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+            <div className="container marketing">
+                <div className="row">
+                    <div className="col-lg-4">
+                        <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777" /><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+
+                        <h2>Heading</h2>
+                        <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
+                        <p><a className="btn btn-secondary" href="#">View details &raquo;</a></p>
+                    </div>
+                    <div className="col-lg-4">
+                        <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777" /><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+
+                        <h2>Heading</h2>
+                        <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
+                        <p><a className="btn btn-secondary" href="#">View details &raquo;</a></p>
+                    </div>
+                    <div className="col-lg-4">
+                        <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777" /><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+
+                        <h2>Heading</h2>
+                        <p>And lastly this, the third column of representative placeholder content.</p>
+                        <p><a className="btn btn-secondary" href="#">View details &raquo;</a></p>
+                    </div>
                 </div>
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
-        </div>
-        </div>
-    )
+            </div>
+            )
 
 }
 
-export default Home
+            export default Home
