617e67073ee6919de43ff7f0d71d33dde7d50b9d
Matheus M. Reis <mmr94@live.com>
2021-06-24 09:28:19 -0300

    ajustes nos cards de produtos

--- src/Projeto/Componentes/Cards/CardProdutos.jsx
+++ src/Projeto/Componentes/Cards/CardProdutos.jsx
@@ -1,13 +1,23 @@
 import { Link } from "react-router-dom"
 
-const CardProduto = ({ nome, preco, categoria, id}) => { 
-    return (
-        <div>
-            <h3> {nome}</h3>
-            <div> {preco}</div>
 
-            <Link to={`/produtos/${id}`}>Ver detalhes </Link>
+const CardProduto = ({ nome, preco,descricao,categoria, id}) => { 
+    return (
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col">
+                <div class="card">
+                    <img src="" class="card-img-top" alt="..."/>
+                        <div class="card-body">
+                            <h5 class="card-title">{nome} - R$ {preco}</h5>
+                                 <p class="card-text">{descricao}</p>
+                        </div>
+                </div>
+            </div>
         </div>
+       
     )
 }
-export default CardProduto
+export default CardProduto;
+
+
+
