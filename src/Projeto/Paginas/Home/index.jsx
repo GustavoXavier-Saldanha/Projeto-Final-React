c35032003b83c5d09376649f7163d738fda08fb6
Gustavo Xavier <gustavosaldxav@gmail.com>
2021-06-24 10:00:45 -0300

    carrousel

--- src/Projeto/Paginas/Home/index.jsx
+++ src/Projeto/Paginas/Home/index.jsx
@@ -1,7 +1,8 @@
 import './estilo.css'
 import img1 from './LogoCortadoHome.png'
-import img4 from './imagem menor home.jpg'
-import img3 from './ImagemNikeIMG2.jpg'
+import img3 from './nikePosterCarrousel.jpg'
+// import img4 from './imagem menor home.jpg'
+// import img3 from './ImagemNikeIMG2.jpg'
 import img2 from './maxresdefault.jpg'
 
 const Home = () => {
@@ -10,32 +11,32 @@ const Home = () => {
 
     return (
         <div className="home">
-            <img src={img1} className="Logo-Tela"/>
+
         <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-indicators">
+            <div className="carousel-indicators">
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
             </div>
 
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src={img2} class="d-block w-100" alt="..." />
+            <div className="carousel-inner">
+                <div className="carousel-item active">
+                    <img src={img2} className="d-block w-90" />
                 </div>
-                <div class="carousel-item">
-                    <img src={img3} class="d-block w-100" alt="..." />
+                <div className="carousel-item">
+                    <img src={img3} className="d-block w-90" />
                 </div>
-                <div class="carousel-item">
+                {/* <div class="carousel-item">
                     <img src={img4} class="d-block w-100" alt="..." />
-                </div>
+                </div> */}
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
             </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
             </button>
         </div>
         </div>
