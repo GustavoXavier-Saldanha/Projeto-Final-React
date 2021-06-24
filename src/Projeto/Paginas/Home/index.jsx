c4aff8a3aee1b80b2337bc8aad17a6b4a74ad972
Matheus M. Reis <mmr94@live.com>
2021-06-24 13:49:21 -0300

    alterando a home

--- src/Projeto/Paginas/Home/index.jsx
+++ src/Projeto/Paginas/Home/index.jsx
@@ -1,7 +1,7 @@
 import './estilo.css'
 import img1 from './LogoCortadoHome.png'
 import img3 from './nikePosterCarrousel.jpg'
-// import img4 from './imagem menor home.jpg'
+import img4 from './imagem menor home.jpg'
 // import img3 from './ImagemNikeIMG2.jpg'
 import img2 from './maxresdefault.jpg'
 
@@ -40,21 +40,20 @@ const Home = () => {
             <div className="container marketing">
                 <div className="row">
                     <div className="col-lg-4">
-                        <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777" /><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+                        <img className="bd-placeholder-img rounded-circle" width="140" height="140" src={img4} alt="" />
                         
                         <h2>Heading</h2>
                         <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
                         <p><a className="btn btn-secondary" href="#">View details &raquo;</a></p>
                     </div>
                     <div className="col-lg-4">
-                        <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777" /><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
-
+                    <img className="bd-placeholder-img rounded-circle" width="140" height="140" src={img4} alt="" />
                         <h2>Heading</h2>
                         <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
                         <p><a className="btn btn-secondary" href="#">View details &raquo;</a></p>
                     </div>
                     <div className="col-lg-4">
-                        <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777" /><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+                    <img className="bd-placeholder-img rounded-circle" width="140" height="140" src={img4} alt="" />
 
                         <h2>Heading</h2>
                         <p>And lastly this, the third column of representative placeholder content.</p>
