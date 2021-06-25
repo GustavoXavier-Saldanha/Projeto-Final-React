2de2b6335576dbb85a53d1d66c53b61b41652e74
Gustavo Xavier <gustavosaldxav@gmail.com>
2021-06-24 23:48:21 -0300

    atualização produtos

--- src/Projeto/Paginas/FinalizarPedido/index.jsx
+++ src/Projeto/Paginas/FinalizarPedido/index.jsx
@@ -1,23 +1,18 @@
-<form className="needs-validation">
-    <div className="my-3">
-        <div className="form-check">
-            <input id="credit" name="paymentMethod" type="radio" className="form-check-input" checked required />
-            <label className="form-check-label" for="credit">Credit card</label>
-        </div>
-        <div className="form-check">
-            <input id="debit" name="paymentMethod" type="radio" className="form-check-input" required />
-            <label className="form-check-label" for="debit">Debit card</label>
-        </div>
-        <div className="form-check">
-            <input id="paypal" name="paymentMethod" type="radio" className="form-check-input" required />
-            <label className="form-check-label" for="paypal">PayPal</label>
-        </div>
+import './estilo.css';
+// import { useHistory } from "react-router-dom";
+// import http from '../../Componentes/http';
+// import { Link } from 'react-router-dom'
 
-        <hr className="my-4" />
-
-        <button className="w-100 btn btn-primary btn-lg" type="submit">Continue to checkout</button>
+const FinalizaPedido = () => {
 
 
 
-    </div >
-</form>
+
+       
+
+    return (
+        <h1></h1>
+    )
+}
+
+export default FinalizaPedido;
