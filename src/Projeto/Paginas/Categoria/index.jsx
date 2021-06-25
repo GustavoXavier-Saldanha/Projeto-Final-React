c18f847debb7bd1fa324648495267d5d75f6bd3e
Matheus M. Reis <mmr94@live.com>
2021-06-25 09:37:57 -0300

    alterando categoria

--- src/Projeto/Paginas/Categoria/index.jsx
+++ src/Projeto/Paginas/Categoria/index.jsx
@@ -17,17 +17,21 @@ const Categorias = () => {
     return (
         <div className="container">
             <div className="col-8">
+                <div>
+                    <h4>Adicionar Categoria</h4>
+                    <Link to="/categoria/adicionar" ><button className="btn btn-dark mt-3 block">Criar Categoria</button></Link>
+                </div>
                 <table className="table table-hover">
                     <thead>
                         <tr>
                             <th>Categorias</th>
                             <th>Informações</th>
                             <th>Produtos desta categoria</th>
-                            <th>Criar categoria</th>
+                            
                         </tr>
                     </thead>
                     <tbody>
-                        {categorias.map(categoria => <tr key={categoria.id}><td>{categoria.nome}</td><td>{categoria.descricao}</td><td><Link>Detalhes</Link></td><td><Link to="/categoria/adicionar" >Adicionar</Link></td></tr>)}
+                        {categorias.map(categoria => <tr key={categoria.id}><td>{categoria.nome}</td><td>{categoria.descricao}</td><td><Link>Detalhes</Link></td></tr>)}
                     </tbody>
                 </table>
             </div>
